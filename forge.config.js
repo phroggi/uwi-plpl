fd49b855ac407ddc36fa33e7e156fcf1097d2317
phroggi <aaronah02@icloud.com>
2022-08-21 23:51:41 -0700

    modify config

---
+++ forge.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  packagerConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "school"
+      }
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"]
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {}
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {}
+    }
+  ],
+  plugins: [
+    [
+      "@electron-forge/plugin-webpack",
+      {
+        devContentSecurityPolicy: `default-src 'self' 'unsafe-inline' data:; connect-src *; script-src 'self' 'unsafe-eval' 'unsafe-inline' data:; img-src * data:`,
+        mainConfig: "./webpack/main.config.js",
+        renderer: {
+          config: "./webpack/renderer.config.js",
+          entryPoints: [
+            {
+              html: "./src/render/index.html",
+              js: "./src/render/index.tsx",
+              name: "main_window",
+              preload: {
+                js: "./src/preload.ts"
+              }
+            }
+          ]
+        }
+      }
+    ]
+  ]
+};
---
+++ webpack/main.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: "./src/main/index.ts",
+  module: {
+    rules: [
+      ...require("./rules"),
+      {
+        test: /\.(svg|jpg|jpeg|png|ico|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[hash][ext][query]"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"]
+  }
+};
